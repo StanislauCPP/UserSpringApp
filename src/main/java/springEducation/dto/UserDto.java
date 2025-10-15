23500af8552c1d3781f77637c1e9bfff95c30e7c
Stas <stanislaukasach@gmail.com>
2025-10-15 14:28:14 +0300

    Schema description was added in UserDto

--- src/main/java/springEducation/dto/UserDto.java
+++ src/main/java/springEducation/dto/UserDto.java
@@ -1,5 +1,6 @@
 package springEducation.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.annotation.Nullable;
 import lombok.*;
 
@@ -11,19 +12,25 @@ import java.time.LocalDate;
 @RequiredArgsConstructor
 @EqualsAndHashCode
 @ToString
+@Schema(description = "User data transfer object")
 public class UserDto {
 
+	@Schema(description = "Primary key", example = "5", accessMode = Schema.AccessMode.READ_ONLY)
 	@Nullable
 	private Long id;
 
+	@Schema(description = "User name", example = "Vasya")
 	@NonNull
 	private String name;
 
+	@Schema(description = "User email", example = "vasya@email.net")
 	@NonNull
 	private String email;
 
+	@Schema(description = "User age", example = "50")
 	@NonNull
 	private Integer age;
 
+	@Schema(description = "Date of creating user", example = "2025-10-10")
 	private LocalDate createdAt = LocalDate.now();
 }
